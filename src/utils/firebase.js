bcb33de0dfa371aab08fd322b22138e7d5b60114
Grigory Tupikov <gtupikov@jetlend.ru>
2024-03-03 19:34:22 +0200

    FavsApp firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCtdX2q3pFv0dHjNEsDaicaPnIeDZ4qUiU",
-  authDomain: "cofegeo.firebaseapp.com",
-  projectId: "cofegeo",
-  storageBucket: "cofegeo.appspot.com",
-  messagingSenderId: "457717190004",
-  appId: "1:457717190004:web:73d76d6892b4c97e017021",
-  measurementId: "G-6WQTMHTLM8"
+  apiKey: "AIzaSyDm6_Anp_qZhxTyhg_FbaYA4A4Y5mm3Z8A",
+  authDomain: "favs-85f44.firebaseapp.com",
+  projectId: "favs-85f44",
+  storageBucket: "favs-85f44.appspot.com",
+  messagingSenderId: "1076432211496",
+  appId: "1:1076432211496:web:0035fa99ac28c729f4ef1d",
+  measurementId: "G-68E8CSFG9N"
 };
 
 // Initialize Firebase
